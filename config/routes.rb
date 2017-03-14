8cd4e1e0fbdb7c99231396d8a0e1ce7fd8564252
naotoshimokawa <ssshimokawa@gmail.com>
2017-03-14 23:28:15 +0900

    リファクタリングしてみたよ

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'notes/new'
-  post '/notes' => 'notes#create'
-  get '/notes' => 'notes#index'
-  get '/notes/:id' => 'notes#show',as: 'note'
-  patch '/notes/:id/' => 'notes#update', as: 'update_note'
-  get '/notes/:id/edit' => 'notes#edit', as: 'edit_note'
-  delete '/notes/:id' =>'notes#edit', as: 'destroy_note'
+  
+  resources :notes
 
   root 'home#top'
   get '/about' => 'home#about'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
