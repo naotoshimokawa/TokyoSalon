acd42000d782129ee2f4b4df167bc479ca1a9b95
naotoshimokawa <ssshimokawa@gmail.com>
2017-03-14 19:58:58 +0900

    機能を追加してみたよ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'notes/new'
+  post '/notes' => 'notes#create'
+  get '/notes' => 'notes#index'
+  get '/notes/:id' => 'notes#show',as: 'note'
+  patch '/notes/:id/' => 'notes#update', as: 'update_note'
+  get '/notes/:id/edit' => 'notes#edit', as: 'edit_note'
+  delete '/notes/:id' =>'notes#edit', as: 'destroy_note'
+
   root 'home#top'
   get '/about' => 'home#about'
 
